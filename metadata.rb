3b5561caa1c7e252588a3a00b8fa072ab0086dcd
Marco Mancuso <marco.mancuso@alfresco.com>
2017-05-10 10:51:36 +0100

    adding chef-version for foodcritic

--- metadata.rb
+++ metadata.rb
@@ -9,6 +9,8 @@ issues_url       'https://github.com/Alfresco/chef-alfresco-webserver/issues'
 source_url       'https://github.com/Alfresco/chef-alfresco-webserver'
 version          '0.7.4'
 
+chef_version '~> 12' if respond_to?(:chef_version)
+
 supports 'centos', '>= 7.0'
 supports 'redhat', '>= 7.0'
 
